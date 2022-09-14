fa034884a7b7a908495d22c5f96cd2bac5dbd462
junha <goodjunha@gmail.com>
2022-09-14 20:20:14 +0900

    😱:: skip

--- RxSwiftPractice/FIlteringOperators.playground/Contents.swift
+++ RxSwiftPractice/FIlteringOperators.playground/Contents.swift
@@ -40,3 +40,39 @@ Observable.of(1,2,3,4,5,6,7,8)
         print($0)
     })
     .disposed(by: disposeBag)
+
+print("------skip------")
+Observable.of("🥸","😀","😁","😇","😊","🥺")
+    .skip(5)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("------skipWhile------")
+Observable.of("🥸","😀","😁","😇","😊","🥺","🤬","🐶","🥰")
+    .skip(while: {
+        $0 != "🐶"
+    })
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("------skipUntil------")
+//기존 observable이 실행되고 그때부터 실행
+let 손님 = PublishSubject<String>()
+let 문여는시간 = PublishSubject<String>()
+
+손님
+    .skip(until: 문여는시간)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+손님.onNext("🥲")
+손님.onNext("😜")
+
+문여는시간.onNext("땡!")
+손님.onNext("😢")
