c89260e6aa61ef371dc18ddb0d6c93b21c04dbcf
junha <goodjunha@gmail.com>
2022-09-14 20:03:12 +0900

    🚫:: filter

--- RxSwiftPractice/FIlteringOperators.playground/Contents.swift
+++ RxSwiftPractice/FIlteringOperators.playground/Contents.swift
@@ -32,3 +32,11 @@ tooUp.onNext("🔈") //index0
 tooUp.onNext("🔈") //index1
 tooUp.onNext("⭐️") //index2
 tooUp.onNext("🔈") //index3
+
+print("------filter------")
+Observable.of(1,2,3,4,5,6,7,8)
+    .filter { $0 % 2 == 0}
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
