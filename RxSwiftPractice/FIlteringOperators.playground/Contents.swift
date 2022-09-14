e63d0eadfee72b97a9583131fa55689ea6f9143a
junha <goodjunha@gmail.com>
2022-09-14 20:00:22 +0900

    🔈:: elementAt

--- RxSwiftPractice/FIlteringOperators.playground/Contents.swift
+++ RxSwiftPractice/FIlteringOperators.playground/Contents.swift
@@ -16,3 +16,19 @@ let 취침모드 = PublishSubject<String>()
 취침모드.onNext("🔈")
 
 취침모드.onCompleted()
+
+print("------elementAt------")
+
+let tooUp = PublishSubject<String>()
+
+tooUp
+    .element(at: 2)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+tooUp.onNext("🔈") //index0
+tooUp.onNext("🔈") //index1
+tooUp.onNext("⭐️") //index2
+tooUp.onNext("🔈") //index3
